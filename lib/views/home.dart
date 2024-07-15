1963260a04ab09244afb9e52628ac336e2dbd2bb
Similoluwa <osimiloluwa9@gmail.com>
2024-07-15 01:02:33 +0100

    replaced with swiper

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,7 +1,7 @@
 // import 'package:carousel_slider/carousel_slider.dart';
 import 'package:card_swiper/card_swiper.dart';
-import 'package:flutter/material.dart' hide CarouselController;
-import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:flutter/material.dart' ;
+
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:spotify/controllers/providers/songs_provider.dart';
